57d490585dfbbbb14794060d82ceff7e5eb1491b
kanxueke <1364075165@qq.com>
2019-01-09 18:53:32 +0800

    读取properties文件

--- FileIODemo/src/main/java/com/practice/FileIO.java
+++ FileIODemo/src/main/java/com/practice/FileIO.java
@@ -1,12 +1,12 @@
 package com.practice;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+
 public class FileIO 
 {
     public static void main( String[] args ){
-    	
+    	//创建file对象
+    	File file =new File("");
+    	//判断file对象是否存在
     }
 }
