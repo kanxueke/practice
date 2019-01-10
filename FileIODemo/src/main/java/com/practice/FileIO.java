2077f6619d75d907f172fc523fa1c64ba6a11d20
kanxueke <1364075165@qq.com>
2019-01-10 14:42:53 +0800

    1

--- FileIODemo/src/main/java/com/practice/FileIO.java
+++ FileIODemo/src/main/java/com/practice/FileIO.java
@@ -2,11 +2,12 @@ package com.practice;
 
 import java.io.File;
 
-public class FileIO 
+public class FileIO
 {
     public static void main( String[] args ){
     	//创建file对象
     	File file =new File("");
     	//判断file对象是否存在
+    	System.out.println("11111111");
     }
 }
